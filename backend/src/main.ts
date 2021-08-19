70484bbf9baae414e89b70659195c96230b989d5
Sergio Lopez Diaz <sldiaz04us@gmail.com>
2021-08-19 15:56:40 -0500

    fix(backend): payloadTooLargeError
    - Fix PayloadTooLargeError: request entity too large.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,23 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import * as express from 'express';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+
+  app.use(express.json({ limit: '50mb' }));
+  app.use(
+    express.urlencoded({
+      limit: '50mb',
+      extended: true,
+      parameterLimit: 50000,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
